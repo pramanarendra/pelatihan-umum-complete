f091213e3730a7cd1498cf4c125f2bfcf1b6dcac
pramanarendra <albertushandypramana@gmail.com>
2022-10-28 18:31:59 +0700

    inisialisasi

---
+++ portfolio/script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', () => {
+    document.getElementById('menu').classList.toggle("hidden");
+    document.getElementById('menu').classList.toggle("flex");
+})
